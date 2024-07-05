b10d320442871c1e5eac8ab2fef63465f7157c0f
ivanlie <ivan.lie@meditap.id>
2024-07-05 16:26:41 +0700

    clean up project

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,7 @@
-import { createPost, getPosts } from '@/server/actions/posts'
-
 export default async function Home() {
-	const data = await getPosts()
-
 	return (
 		<main>
-			{data.map((post) => (
-				<p key={post.id}>{post.title}</p>
-			))}
-			<form action={createPost}>
-				<input
-					type="text"
-					name="title"
-					placeholder="Title"
-					className="bg-black text-white"
-				/>
-				<button type="submit">Submit</button>
-			</form>
+			<h1>Homepage</h1>
 		</main>
 	)
 }
